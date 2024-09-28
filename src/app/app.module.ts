ce10d5dbc4c79b769371331fef286b39aa0f210e
M.V. Bego <marcus-gomes-v@users.noreply.github.com>
2024-09-29 01:26:01 +0200

    chore: set uptime

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,16 @@ import { ExampleModule } from './modules/example/example.module';
 import { SiteReportModule } from './modules/site-report/site-report.module';
 import { ConclusionModule } from './modules/conclusion/conclusion.module';
 import { ProviderModule } from './modules/provider/provider.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [DatabaseModule, ExampleModule, SiteReportModule, ConclusionModule, ProviderModule],
+  imports: [
+    DatabaseModule,
+    ExampleModule,
+    SiteReportModule,
+    ConclusionModule,
+    ProviderModule,
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
