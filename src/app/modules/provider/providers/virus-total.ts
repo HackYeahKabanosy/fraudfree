6b4091016486f56e1048d7045371852f0cb10e51
M.V. Bego <marcus-gomes-v@users.noreply.github.com>
2024-09-28 22:23:10 +0200

    feat: Implement simple provider

--- src/app/modules/provider/providers/virus-total.ts
+++ src/app/modules/provider/providers/virus-total.ts
@@ -1,8 +1,37 @@
+import axios from 'axios';
+
 export class VirusTotal {
+  private apiKey: string;
+
+  constructor() {
+    // Store your VirusTotal API key
+    this.apiKey =
+      'b4a6e161f5b9c62a6ad9a217ccfd72c5aad1e3b1e4e4ce98ba2782532f1dbc52';
+  }
+
   async factory(url: string) {
-    return {
-      analizedUrl: url,
-      reason: 'Not implemented',
-    };
+    try {
+      const response = await this.requestData(url);
+      return response.data.data;
+    } catch (error) {
+      return {
+        analyzedUrl: url,
+        error: error.message || 'Error during request',
+      };
+    }
+  }
+
+  private async requestData(domain: string) {
+    const apiUrl = `https://www.virustotal.com/api/v3/domains/${domain}`;
+
+    // Make a GET request using Axios
+    const response = await axios.get(apiUrl, {
+      headers: {
+        accept: 'application/json',
+        'x-apikey': this.apiKey,
+      },
+    });
+
+    return response;
   }
 }
