4145c5a51c02fd23e397289e947d9357775d9e65
M.V. Bego <marcus-gomes-v@users.noreply.github.com>
2024-09-29 10:40:47 +0200

    chore: implement better message

--- src/app/modules/conclusion/conclusion.controller.ts
+++ src/app/modules/conclusion/conclusion.controller.ts
@@ -24,6 +24,16 @@ export class ConclusionController {
           : 'Danger';
   }
 
+  private convertScanIntoMessage(scamProbability: number): string {
+    return scamProbability < 2
+      ? 'This e-commerce site has a strong reputation and is confirmed to be legitimate. You can browse and make purchases with confidence.'
+      : scamProbability < 4
+        ? 'This website appears trustworthy based on its domain history and online reputation. If it’s a marketplace, it’s a good idea to check the credibility of the individual seller.'
+        : scamProbability < 6
+          ? "This website raises some concerns. It’s important to verify its authenticity, especially if you haven't heard of this store before. Look for well-known brand indicators and be cautious when sharing personal information."
+          : 'This website shows multiple signs of being a potential scam. Information is limited or unclear, and there are several red flags that suggest it may not be safe to use. Proceed with extreme caution, or avoid using this site altogether.';
+  }
+
   buildResponse(data: Conclusion): ConclusionResponseDto {
     return data
       ? {
@@ -32,7 +42,7 @@ export class ConclusionController {
           status: this.converScanIntoSignal(data.scamProbability),
           scamProbability: data.scamProbability,
           scale: data.scale,
-          conclusion: data.conclusion,
+          conclusion: this.convertScanIntoMessage(data.scamProbability),
           keypoints: data.keypoints,
           customerReviews: data.customerReviews,
           createdAt: data.createdAt,
